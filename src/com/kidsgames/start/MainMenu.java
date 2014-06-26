11430626a07f2c171c2922b80e851bae5403192a
AlexZaiats <alex.zaiats@gmail.com>
2014-06-27 00:25:28 +0300

    added new name for app

--- src/com/kidsgames/start/MainMenu.java
+++ src/com/kidsgames/start/MainMenu.java
@@ -28,9 +28,13 @@ import com.vikinc.coloring.R;
 
 public class MainMenu extends Activity implements OnClickListener{
 	
-//	static final String ITEM_SKU = "com.pack2";
-	static final String ITEM_SKU ="buy.pack2";
+	private String publicKey = "MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAglVFlL6qyAYVOGjT9Rnf+TdoraLGXfq8SMPWrVAEbPG0X/1kRsJnVnJZFspsmugwqdNRUy+BwVcD01Pmez/yBvhSdQmNJlrN5F/X+BuWMRyeOzttb8SegTeLbKVB8+OeRXoJtC8Wocgki2/axUhASYpNJNGgAXA2NcACkNOh5jHNfbb0sleWQ7ta6zpZWs3wOhB1eXuHcFTW81qUfftaL9hUzhc8Vzib7Px5Xy+BpUJVoHNxvTvpwU3qQ9hHJYUR4Y23d8Kkqn2qEm6+di+y07LAgGcWb9toeiEALNtHMyDCAbH8/2+5hEMEPb0vIILYJLDS9CXecicZElwzWxK7DQIDAQAB";
+
+	
+	private final String ITEM_SKU = "com.pack2";
+//	static final String ITEM_SKU ="buy.pack2";
 //	static final String ITEM_SKU = "android.test.purchased";
+//	static final String ITEM_SKU = "pack2";
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -38,8 +42,7 @@ public class MainMenu extends Activity implements OnClickListener{
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.main_menu);
 		
-		 bindService(new 
-			        Intent("com.android.vending.billing.InAppBillingService.BIND"),
+		 bindService(new Intent("com.android.vending.billing.InAppBillingService.BIND"),
 			                mServiceConn, Context.BIND_AUTO_CREATE);
 		 
 		findViewById(R.id.pack1_button).setOnClickListener(this);
@@ -49,7 +52,6 @@ public class MainMenu extends Activity implements OnClickListener{
         tracker.send(MapBuilder.createAppView().set(Fields.SCREEN_NAME, "Home Screen").build());
         
         BugSenseHandler.initAndStartSession(MainMenu.this, "718d6664");
-        
 	}
 
 	@Override
@@ -78,11 +80,7 @@ public class MainMenu extends Activity implements OnClickListener{
 		        	    .createEvent("Buy", "pack", "no_account", null)
 		        	    .build());
 			}
-			/*
-			intent = new Intent(this, DrawerSliderActivity.class);
-			intent.putExtra("pack", "pack2");
-			startActivity(intent);
-			*/
+
 			break;
 		}
 	}
@@ -99,24 +97,30 @@ public class MainMenu extends Activity implements OnClickListener{
 	   public void onServiceConnected(ComponentName name, 
 	      IBinder service) {
 	       mService = IInAppBillingService.Stub.asInterface(service);
-			try{
-				Bundle ownedItems = mService.getPurchases(3, getPackageName(), "inapp", null);
+	       
+	       try{
+				Bundle ownedItems = mService.getPurchases(3, getPackageName(), "inapp", "bGoa+V7g/yqDXvKRqq+JTFn4uQZbPiQJo4pf9RzJ");
 				int response = ownedItems.getInt("RESPONSE_CODE");
 				if (response == 0) {
 	
 				   ArrayList<String> purchaseDataList = 
 				      ownedItems.getStringArrayList("INAPP_PURCHASE_DATA_LIST");
 
-				   for (String s : purchaseDataList)
-					   Log.d("Likers" , "Resp : " + s);
+				   for (int i = 0; i < purchaseDataList.size(); ++i) {
+				      String purchaseData = purchaseDataList.get(i);
+		
+				   } 
 
+				   // if continuationToken != null, call getPurchases again 
+				   // and pass in the token to retrieve more items
 				}
 		
 			}
-			catch (Exception e)
-			{
-				
-			}
+	       catch(Exception e)
+	       {
+	    	   Log.d("Likers" , "E : " + e);
+	       }
+	       
 	   }
 	};
 	
@@ -139,9 +143,6 @@ public class MainMenu extends Activity implements OnClickListener{
 			        	    .createEvent("Buy", "pack", "item_bouth", null)
 			        	    .build());
 			        
-					
-		         //   int response = mService.consumePurchase(3, getPackageName(), jo.getString("purchaseToken"));
-		        //    Log.d("Likers" , "Response : "  +response);
 		          }
 		          catch (Exception e) {
 		             Log.d("Likers","Failed to parse purchase data.");
@@ -154,8 +155,6 @@ public class MainMenu extends Activity implements OnClickListener{
 	@Override
 	public void onDestroy() {
 		super.onDestroy();
-
-
 		    if (mServiceConn != null) {
 		    	unbindService(mServiceConn);
 		    }   
